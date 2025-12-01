d503437e6adc485a8d39198298f15fa05e3184d4
Ssuwimon <maynp.08@gmail.com>
2025-12-01 21:55:43 +0700

    ENGCE174-jubbb

--- Lab 1/hw1.java
+++ Lab 1/hw1.java
@@ -1,17 +1,18 @@
-package homework;
-
 import java.util.Scanner;
 
-public class hw1 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter number ");
-		int n1 = sc.nextInt();
-		
-		System.out.print("Enter number ");
-		int n2 = sc.nextInt();
-		
-		int sum = n1 + n2;
-		System.out.print("is you number :"+ n1 + "\n" +"it you number :" +n2 + "\n" + "sum :" + sum );
-		}
+public class Hw1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum = " + sum);
+    }
 }
