a447e7597415f526b3fa91f0cfa5eb9287819664
Ssuwimon <maynp.08@gmail.com>
2025-12-01 22:04:00 +0700

    ENGCE174-jubbb

--- Lab 1.2/hw2.java
+++ Lab 1.2/hw2.java
@@ -1,20 +1,18 @@
-package homework;
-
 import java.util.Scanner;
 
-public class hw2{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-			
-		System.out.print("Enter number ");
-		double n1 = sc.nextDouble();
-		
-		System.out.print("Enter number ");
-		double n2 = sc.nextDouble();
-		
-		double Ra = n1 * n2;
-		
-        System.out.println("Value of rectangle area: " + Ra);
-        
-	}
+public class Hw2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter width: ");
+        double width = sc.nextDouble();
+
+        System.out.print("Enter height: ");
+        double height = sc.nextDouble();
+
+        double area = width * height;
+
+        System.out.println("Area of the rectangle is: " + area);
+    }
 }
