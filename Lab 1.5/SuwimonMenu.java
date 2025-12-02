0cb78699c49d551773ff2648f2dbe80e70030424
Ssuwimon <maynp.08@gmail.com>
2025-12-02 08:27:13 +0700

    ENGCE174-jubbb

--- Lab 1.5/SuwimonMenu.java
+++ Lab 1.5/SuwimonMenu.java
@@ -1,29 +1,28 @@
-package homework;
-
 import java.util.Scanner;
 
-public class hw5{
-	public static void main(String[] args) {
+public class SuwimonMenu {
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("1 = Americano");
         System.out.println("2 = Latte");
         System.out.println("3 = Espresso");
         System.out.println("4 = Mocha");
-        System.out.print("Enter your menu ");
+        System.out.print("Enter your menu: ");
 
-        int m = sc.nextInt();
+        int menu = sc.nextInt();
 
-        if (m < 1 || m > 4) {
-            System.out.println("Error: Invalid Menu");
-        } else if (m == 1) {
+        if (menu == 1) {
             System.out.println("Americano");
-        } else if (m == 2) {
+        } else if (menu == 2) {
             System.out.println("Latte");
-        } else if (m == 3) {
+        } else if (menu == 3) {
             System.out.println("Espresso");
-        } else if (m == 4) {
+        } else if (menu == 4) {
             System.out.println("Mocha");
+        } else {
+            System.out.println("Invalid Menu");
         }
 
         sc.close();
