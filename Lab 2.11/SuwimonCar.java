3924abe06a86daac578fbb344f0d5a9c606a3be4
Ssuwimon <maynp.08@gmail.com>
2025-12-02 11:08:32 +0700

    ENGCE174-jubbb

---
+++ Lab 2.11/SuwimonCar.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Car {
+    private String model;
+    private int year;
+
+    Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int newYear) {
+        this.year = newYear;
+    }
+}
+
+public class SuwimonCar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String model = sc.nextLine();
+        int year = sc.nextInt();
+        int newYear = sc.nextInt();
+
+        Car car = new Car(model, year);
+        car.setYear(newYear);
+
+        System.out.println("Model: " + car.getModel());
+        System.out.println("Year: " + car.getYear());
+        sc.close();
+    }
+}
