e0869e1385b4cdb313b8513e7653457f44320269
Ssuwimon <maynp.08@gmail.com>
2025-12-02 08:19:59 +0700

    ENGCE174-jubbb

---
+++ Lab 1.4/SuwimonGrade4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SuwimonGrade4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter score: ");
+        int score = sc.nextInt();
+
+        if (score >= 80 && score <= 100) {
+            System.out.println("A");
+        } else if (score >= 70) {
+            System.out.println("B");
+        } else if (score >= 60) {
+            System.out.println("C");
+        } else if (score >= 50) {
+            System.out.println("D");
+        } else if (score >= 0) {
+            System.out.println("F");
+        } else {
+            System.out.println("Error: Score must be between 0-100.");
+        }
+
+        sc.close();
+    }
+}
